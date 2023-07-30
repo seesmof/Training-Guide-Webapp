e9090f9f3413374cadfe38b65467222199f32b4f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-30 17:31:06 +0300

    update exercises.js

--- components/exercises.js
+++ components/exercises.js
@@ -12,6 +12,8 @@ const Exercises = [
         video: "pushUps",
         description:
           "Get into a high plank position with hands directly under shoulders and legs extended behind you. Keeping your core engaged, bend elbows to lower chest towards the ground until elbows are at a 90 degree angle. Pause briefly before pressing back up to starting position in a controlled motion.",
+        shortBreak: 90,
+        longBreak: 180,
       },
       {
         name: "Barbell Bench Press",
@@ -21,6 +23,8 @@ const Exercises = [
         video: "benchPress",
         description:
           "Lie back on a flat bench holding a barbell above your chest with an overhand grip slightly wider than shoulder width. Lower bar to chest by bending elbows, keeping them tucked in at your sides. Touch bar to chest then press back up to starting position by extending arms.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Couch Dips",
@@ -30,6 +34,8 @@ const Exercises = [
         video: "benchDip",
         description:
           "Sit on the edge of a sturdy couch or chair, placing hands behind you on the edge with fingers pointing forward. Walk feet out and straighten arms to lift hips off the couch into an extended position. Bend elbows to lower body towards floor, then straighten to raise back up.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Lean-Away Dumbbell Lateral Raise",
@@ -39,6 +45,8 @@ const Exercises = [
         video: "lateralRaise",
         description:
           "Stand holding dumbbells by your sides with palms facing inwards. Keeping a slight bend in elbows, raise both arms straight out to the sides up to shoulder height, leaning torso slightly backwards. Pause then lower back down to starting position in a controlled motion.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Dumbbell Triceps",
@@ -48,6 +56,8 @@ const Exercises = [
         video: "dbTricep",
         description:
           "Stand holding a dumbbell in each hand above your head with arms extended. Keeping upper arms stationary, bend elbows to lower dumbbells behind head until forearms are parallel to ground. Straighten arms back to starting position and repeat.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Standing Dumbbell Shoulder Press",
@@ -57,6 +67,8 @@ const Exercises = [
         video: "shoulderPress",
         description:
           "Stand holding dumbbells at shoulder height with palms facing forward. Press dumbbells directly upwards by extending arms until they are overhead, being careful not to arch back. Slowly lower back to shoulder height and repeat.",
+        shortBreak: 60,
+        longBreak: 120,
       },
     ],
   },
@@ -73,6 +85,8 @@ const Exercises = [
         video: "pullUps",
         description:
           "Grasp pull-up bar with an overhand grip slightly wider than shoulder width. Hang from bar with arms fully extended. Pull yourself up until your chin clears the bar, then lower back down with control. For weighted, use a weight belt or hold a dumbbell between feet.",
+        shortBreak: 90,
+        longBreak: 180,
       },
       {
         name: "Barbell Biceps Curls",
@@ -82,6 +96,8 @@ const Exercises = [
         video: "bicepCurls",
         description:
           "Stand holding a barbell with an underhand shoulder width grip. Keeping upper arms stationary at your sides, curl bar up towards shoulders by bending elbows. Squeeze biceps at top then lower back down slowly with control.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Bent Over Dumbbell Rows",
@@ -91,6 +107,8 @@ const Exercises = [
         video: "dbRows",
         description:
           "Hinge forward from hips until back is nearly parallel to floor, keeping knees slightly bent. Holding a dumbbell in each hand with palms facing in, pull dumbbells up towards sides of chest by driving elbows back. Lower under control then repeat.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Plank with Straight Hands",
@@ -100,6 +118,8 @@ const Exercises = [
         video: "plank",
         description:
           "Get into a straight arm plank position balancing on forearms and toes. Keeping body in straight line, raise up onto hands by straightening arms. Hold for a few seconds then lower back down onto forearms and repeat.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Abs V-Ups",
@@ -109,6 +129,8 @@ const Exercises = [
         video: "vUps",
         description:
           "Lie on floor with legs extended and arms reaching overhead. Engaging core, simultaneously lift legs and torso up off floor, reaching hands towards feet. Aim to form a V shape with your body. Slowly lower back down to starting position with control.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Bent Over Barbell Rows",
@@ -118,6 +140,8 @@ const Exercises = [
         video: "bbRows",
         description:
           "Hinge at hips to bend torso forward, holding barbell with knees slightly bent. Keeping back flat, pull barbell up towards lower chest by driving elbows back and squeezing shoulder blades. Lower under control and repeat.",
+        shortBreak: 60,
+        longBreak: 120,
       },
     ],
   },
@@ -134,6 +158,8 @@ const Exercises = [
         video: "dbSquats",
         description:
           "Stand holding dumbbells at your sides with feet shoulder width. Initiate movement by pushing hips back and bending knees to lower into a squat. Descend until thighs are parallel or below knee level, then press through heels to stand back up.",
+        shortBreak: 90,
+        longBreak: 180,
       },
       {
         name: "Dumbbell Calves Raises",
@@ -143,6 +169,8 @@ const Exercises = [
         video: "dbCalves",
         description:
           "Stand holding dumbbells by your sides with feet hip width. Rise up onto balls of feet, lifting heels as high as possible. Pause at top then slowly lower back down to starting position with control.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Barbell Deadlifts",
@@ -152,6 +180,8 @@ const Exercises = [
         video: "bbDeadlifts",
         description:
           "Stand behind barbell midfoot with feet hip width apart. Hinge at hips to grip bar outside legs, back flat. Drive through heels and extend hips and knees to stand up, lifting bar. Lower bar back to floor with control.",
+        shortBreak: 120,
+        longBreak: 240,
       },
       {
         name: "Barbell Hip Thrusts",
@@ -161,6 +191,8 @@ const Exercises = [
         video: "bbHipThrusts",
         description:
           "Sit on ground with shoulders against a bench and barbell over hips. With feet on floor, drive through heels to raise hips, lifting bar. Squeeze glutes at top then lower back down to start.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Dumbbell Lunges",
@@ -170,6 +202,8 @@ const Exercises = [
         video: "dbLunge",
         description:
           "Stand holding dumbbells at your sides. Step forward with one leg, lowering into a lunge by bending both knees to 90 degrees. Push off rear foot to return to standing and alternate legs.",
+        shortBreak: 60,
+        longBreak: 120,
       },
     ],
   },
@@ -186,6 +220,8 @@ const Exercises = [
         video: "dbSquats",
         description:
           "Set bar on upper back and stand with feet shoulder width apart. Send hips back and bend knees to lower into a squat. Descend until thighs are parallel to floor then drive through heels to return to start position.",
+        shortBreak: 90,
+        longBreak: 180,
       },
       {
         name: "Barbell Bench Press",
@@ -195,6 +231,8 @@ const Exercises = [
         video: "benchPress",
         description:
           "Lie back on a flat bench holding barbell above chest with overhand grip. Lower bar to chest by bending elbows out to sides. Touch chest then press bar back up by extending arms.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Pull-Ups",
@@ -204,6 +242,8 @@ const Exercises = [
         video: "pullUps",
         description:
           "Grasp pull-up bar with palms facing forward using overhand grip. Hang with arms extended then pull yourself up until chin clears bar. Lower back down with control and repeat.",
+        shortBreak: 90,
+        longBreak: 180,
       },
       {
         name: "Barbell Deadlifts",
@@ -213,6 +253,8 @@ const Exercises = [
         video: "bbDeadlifts",
         description:
           "Stand behind bar midfoot with hip-width stance. Bend at hips to grip bar outside legs while keeping back flat. Drive through heels and extend hips and knees to stand up, lifting bar. Lower under control.",
+        shortBreak: 120,
+        longBreak: 240,
       },
       {
         name: "Push-Ups",
@@ -222,6 +264,8 @@ const Exercises = [
         video: "pushUps",
         description:
           "In plank position with hands under shoulders, bend elbows to lower chest to ground until elbows are at 90 degree angle. Pause then press back up to starting plank position by extending arms.",
+        shortBreak: 60,
+        longBreak: 120,
       },
       {
         name: "Bent Over Barbell Rows",
@@ -231,6 +275,8 @@ const Exercises = [
         video: "bbRows",
         description:
           "With knees bent, hinge at hips to bend torso forward, holding barbell. Keeping back flat, pull barbell up towards lower chest by driving elbows back and squeezing shoulder blades. Lower under control.",
+        shortBreak: 60,
+        longBreak: 120,
       },
     ],
   },
